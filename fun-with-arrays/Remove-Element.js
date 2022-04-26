b1110389e4791975e925da0efc2af7b176f1dfb4
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-04-26 10:04:40 +0400

    final

---
+++ fun-with-arrays/Remove-Element.js
@@ -0,0 +1,20 @@
+/*
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i -= 1;
+    }
+  }
+
+  return nums.length;
+};
+
+const result = removeElement([3, 2, 2, 3], 3);
+console.log(result);
