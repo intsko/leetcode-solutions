43e8b6dd45cafa208af3505f1b92052dc0dc8d15
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-04-27 10:42:43 +0400

    final

--- fun-with-arrays/Remove-Duplicates-from-Sorted-Array.js
+++ fun-with-arrays/Remove-Duplicates-from-Sorted-Array.js
@@ -5,4 +5,16 @@ Explanation: Your function should return k = 2, with the first two elements of n
 It does not matter what you leave beyond the returned k (hence they are underscores).
 */
 
-var removeDuplicates = function (nums) {};
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i -= 1;
+    }
+  }
+  return nums.length;
+};
+
+const result = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+
+console.log(result); // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
