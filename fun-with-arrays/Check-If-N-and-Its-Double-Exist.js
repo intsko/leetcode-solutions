862ec24f6a39c979c6aa96bbf9598729ea9fd55e
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-04-27 11:03:35 +0400

    final

--- fun-with-arrays/Check-If-N-and-Its-Double-Exist.js
+++ fun-with-arrays/Check-If-N-and-Its-Double-Exist.js
@@ -4,6 +4,15 @@ Output: true
 Explanation: N = 10 is the double of M = 5,that is, 10 = 2 * 5.
 */
 
-var checkIfExist = function (arr) {};
+var checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] * 2 === arr[j] && i !== j) return true;
+    }
+  }
+  return false;
+};
 
-const result = checkIfExist([10, 2, 5, 3]); // true
+const result = checkIfExist([3, 1, 7, 11]); // false
+const result1 = checkIfExist([10, 2, 5, 3]); // true
+console.log(result);
