ee5b151c330224882f51882e3c434f910648b5da
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-04-27 10:48:28 +0400

    init

---
+++ fun-with-arrays/Check-If-N-and-Its-Double-Exist.js
@@ -0,0 +1,9 @@
+/*
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: N = 10 is the double of M = 5,that is, 10 = 2 * 5.
+*/
+
+var checkIfExist = function (arr) {};
+
+const result = checkIfExist([10, 2, 5, 3]); // true
