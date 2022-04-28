a6c492be8aac009eb4914bba4d54d6acb2ff9b0d
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-04-28 09:21:05 +0400

    final

--- fun-with-arrays/Valid-Mountain-Array.js
+++ fun-with-arrays/Valid-Mountain-Array.js
@@ -1,1 +1,15 @@
-var validMountainArray = function (arr) {};
+var validMountainArray = function (arr) {
+  let asc = false;
+  let desc = false;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] == arr[i - 1]) return false;
+    if (arr[i] > arr[i - 1]) {
+      if (desc) return false;
+      asc = true;
+    }
+    if (arr[i] < arr[i - 1]) {
+      desc = true;
+    }
+  }
+  return asc && desc;
+};
