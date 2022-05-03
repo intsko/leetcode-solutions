bb1c303d8404a8a2facdc4cbab7719efeffb711f
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-03 10:58:39 +0400

    final

--- fun-with-arrays/Move-Zeroes.js
+++ fun-with-arrays/Move-Zeroes.js
@@ -3,4 +3,16 @@ Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 */
 
-var moveZeroes = function (nums) {};
+var moveZeroes = function (nums) {
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+  }
+
+  return nums;
+};
+
+const result = moveZeroes([0, 1, 0, 3, 12]);
+console.log(result);
