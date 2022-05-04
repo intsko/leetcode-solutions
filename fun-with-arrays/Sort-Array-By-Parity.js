e0d2cb19bbf38b1dcc1ac12cc0c4d5b261bd4da4
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-04 08:51:58 +0400

    final

--- fun-with-arrays/Sort-Array-By-Parity.js
+++ fun-with-arrays/Sort-Array-By-Parity.js
@@ -4,4 +4,12 @@ Output: [2,4,3,1]
 Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 */
 
-var sortArrayByParity = function (nums) {};
+var sortArrayByParity = function (nums) {
+  const evens = nums.filter((num) => num % 2 === 0);
+  const odds = nums.filter((num) => num % 2 !== 0);
+
+  return evens.concat(odds);
+};
+
+const result = sortArrayByParity([3, 1, 2, 4]);
+console.log(result);
