adc4c291937d71caec16132dfbad73b3a8123b94
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-05 09:03:35 +0400

    init

---
+++ fun-with-arrays/Third-Maximum-Number.js
@@ -0,0 +1,13 @@
+/*
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+*/
+
+var thirdMax = function (nums) {};
+
+const result = thirdMax([3, 2, 1]); // 1
+console.log(result);
