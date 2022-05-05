d2e00476c0ef788dbaffd8b4648ec7e3d750a490
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-05 08:59:51 +0400

    final

--- fun-with-arrays/Height-Checker.js
+++ fun-with-arrays/Height-Checker.js
@@ -7,6 +7,17 @@ expected: [1,1,1,2,3,4]
 Indices 2, 4, and 5 do not match.
 */
 
-var heightChecker = function (heights) {};
+var heightChecker = function (heights) {
+  let sum = 0;
+  const hgts = [...heights];
+  const expected = heights.sort((a, b) => a - b);
 
-const result = heightChecker([1, 1, 4, 2, 1, 3]); // expected: [1,1,1,2,3,4]
+  for (let i = 0; i < expected.length; i++) {
+    if (expected[i] !== hgts[i]) sum += 1;
+  }
+
+  return sum;
+};
+
+const result = heightChecker([1, 1, 4, 2, 1, 3]);
+console.log(result);
