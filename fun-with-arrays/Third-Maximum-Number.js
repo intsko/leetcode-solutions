b35fc7149ae64b7158d47997f41d0560756f4d14
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-05 09:15:44 +0400

    final

--- fun-with-arrays/Third-Maximum-Number.js
+++ fun-with-arrays/Third-Maximum-Number.js
@@ -7,7 +7,10 @@ The second distinct maximum is 2.
 The third distinct maximum is 1.
 */
 
-var thirdMax = function (nums) {};
+var thirdMax = function (nums) {
+  const sorted = [...new Set(nums.sort((a, b) => b - a))];
+  return sorted.length < 3 ? sorted[0] : sorted[2];
+};
 
-const result = thirdMax([3, 2, 1]); // 1
+const result = thirdMax([1, 2, 2, 3]); // 1
 console.log(result);
