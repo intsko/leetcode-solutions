5917eb4706b777f49997064881fbaf514823c19f
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-06 10:29:00 +0400

    init

---
+++ fun-with-arrays/Find-All-Numbers-Disappeared-in-an-Array.js
@@ -0,0 +1,11 @@
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+*/
+
+var findDisappearedNumbers = function (nums) {};
+
+const result = findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]); // [5, 6]
+console.log(result);
