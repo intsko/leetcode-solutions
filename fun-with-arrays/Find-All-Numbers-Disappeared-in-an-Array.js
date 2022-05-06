40703441dd977de4a7d716571839b803c66f3293
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-06 10:48:05 +0400

    final

--- fun-with-arrays/Find-All-Numbers-Disappeared-in-an-Array.js
+++ fun-with-arrays/Find-All-Numbers-Disappeared-in-an-Array.js
@@ -5,7 +5,14 @@ Input: nums = [4,3,2,7,8,2,3,1]
 Output: [5,6]
 */
 
-var findDisappearedNumbers = function (nums) {};
+var findDisappearedNumbers = function (nums) {
+  const missed = [];
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!nums.includes(i)) missed.push(i);
+  }
+  return missed;
+};
 
 const result = findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]); // [5, 6]
 console.log(result);
