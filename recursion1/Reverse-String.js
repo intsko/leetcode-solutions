bbd5d6e6db61614bcdc92ac16a0c823bb68c68a3
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-07 11:51:06 +0400

    init

---
+++ recursion1/Reverse-String.js
@@ -0,0 +1,9 @@
+/*
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+*/
+
+var reverseString = function (s) {};
+
+const result = reverseString(["h", "e", "l", "l", "o"]);
+console.log(result);
