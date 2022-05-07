512039225dc6f9d5a7b3c613b9a4b908be39f8cb
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-07 11:58:58 +0400

    init

---
+++ recursion1/Swap-Nodes-in-Pairs.js
@@ -0,0 +1,11 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+*/
+
+var swapPairs = function (head) {};
+
+const result = swapPairs([1, 2, 3, 4]);
+console.log(result);
