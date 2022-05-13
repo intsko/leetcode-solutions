15f9ea5163aed5d43e529ec24c28353c6283d65d
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-13 08:58:08 +0400

    final

--- array-and-string/Find-Pivot-Index.js
+++ array-and-string/Find-Pivot-Index.js
@@ -7,7 +7,19 @@ Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
 Right sum = nums[4] + nums[5] = 5 + 6 = 11
 */
 
-var pivotIndex = function (nums) {};
+var pivotIndex = function (nums) {
+  const total = nums.reduce((acc, curr) => acc + curr);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const rightSum = total - leftSum;
+    leftSum += nums[i];
+
+    if (leftSum === rightSum) return i;
+  }
+
+  return -1;
+};
 
 const result = pivotIndex([1, 7, 3, 6, 5, 6]);
 console.log(result);
