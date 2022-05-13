e2a9b1d1dd02fdae37fbf0a4ec719b87b4754a19
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-13 09:17:16 +0400

    final

--- array-and-string/Largest-Number-At-Least-Twice-of-Others.js
+++ array-and-string/Largest-Number-At-Least-Twice-of-Others.js
@@ -6,7 +6,14 @@ For every other number in the array x, 6 is at least twice as big as x.
 The index of value 6 is 1, so we return 1.
 */
 
-var dominantIndex = function (nums) {};
+var dominantIndex = function (nums) {
+  const max = Math.max(...nums);
+  const maxIndex = nums.indexOf(max);
+  nums.splice(maxIndex, 1);
+  const secondLargest = Math.max(...nums);
+
+  return secondLargest * 2 <= max ? maxIndex : -1;
+};
 
 const result = dominantIndex([3, 6, 1, 0]);
 console.log(result);
