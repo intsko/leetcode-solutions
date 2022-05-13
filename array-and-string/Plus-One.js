9399c1aa94604a01897a266109b5ddec70ad785d
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-13 09:56:21 +0400

    final

--- array-and-string/Plus-One.js
+++ array-and-string/Plus-One.js
@@ -6,7 +6,12 @@ Incrementing by one gives 123 + 1 = 124.
 Thus, the result should be [1,2,4].
 */
 
-var plusOne = function (digits) {};
+var plusOne = function (digits) {
+  return (Number(digits.join("")) + 1)
+    .toString()
+    .split("")
+    .map((str) => Number(str));
+};
 
 const result = plusOne([1, 2, 3]);
 console.log(result);
