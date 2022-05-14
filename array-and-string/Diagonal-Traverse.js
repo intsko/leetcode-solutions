7d46b3d0d07e291a3cffd464dfaded7a434edf92
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-14 10:52:37 +0400

    init

---
+++ array-and-string/Diagonal-Traverse.js
@@ -0,0 +1,13 @@
+/*
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+*/
+
+var findDiagonalOrder = function (mat) {};
+
+const result = findDiagonalOrder([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+console.log(result);
