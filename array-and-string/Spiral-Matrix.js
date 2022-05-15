bf0da773a87669f21f4ef2ca6fa4fdd1553e12f1
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-15 11:11:50 +0400

    final

--- array-and-string/Spiral-Matrix.js
+++ array-and-string/Spiral-Matrix.js
@@ -3,7 +3,41 @@ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
 */
 
-var spiralOrder = function (matrix) {};
+var spiralOrder = function (matrix) {
+  let x = matrix[0].length,
+    y = matrix.length - 1;
+  const res = [];
+  let i = 0,
+    j = -1;
+
+  const collect = () => {
+    res.push(matrix[i][j]);
+  };
+
+  const actionHandler = (type, offset, step) => {
+    for (let k = 0; k < step; k++) {
+      if (type == "i") i += offset;
+      if (type == "j") j += offset;
+      collect();
+    }
+    return step - 1;
+  };
+
+  while (true) {
+    if (!x) break;
+    x = actionHandler("j", 1, x);
+
+    if (!y) break;
+    y = actionHandler("i", 1, y);
+
+    if (!x) break;
+    x = actionHandler("j", -1, x);
+
+    if (!y) break;
+    y = actionHandler("i", -1, y);
+  }
+  return res;
+};
 
 const result = spiralOrder([
   [1, 2, 3],
