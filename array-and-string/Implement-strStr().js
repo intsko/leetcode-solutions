e3203bad60cb75ae7348b551991300213cd07f57
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-16 10:38:57 +0400

    final

--- array-and-string/Implement-strStr().js
+++ array-and-string/Implement-strStr().js
@@ -4,6 +4,9 @@ Output: 2
 */
 
 var strStr = function (haystack, needle) {
+  if (!needle.length) return 0;
+  if (haystack.length < needle.length) return -1;
+
   loop: for (let i = 0; i < haystack.length; i++) {
     for (let j = 0; j < needle.length; j++) {
       if (haystack[i + j] !== needle[j]) {
@@ -13,6 +16,8 @@ var strStr = function (haystack, needle) {
 
     return i;
   }
+
+  return -1;
 };
 
 const result = strStr("hello", "ll");
