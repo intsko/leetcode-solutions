9a5fc957fba69073c575ad8e37b90d770c1c9050
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-18 11:51:07 +0400

    final

---
+++ array-and-string/Reverse-String.js
@@ -0,0 +1,11 @@
+/*
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+*/
+
+var reverseString = function (s) {
+  return s.reverse();
+};
+
+const result = reverseString(["h", "e", "l", "l", "o"]);
+console.log(result);
