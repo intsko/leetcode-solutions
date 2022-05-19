53ba744fd42b0482c1d0619aa7c795c0884a7b11
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-19 08:54:08 +0400

    final

--- array-and-string/Max-Consecutive-Ones.js
+++ array-and-string/Max-Consecutive-Ones.js
@@ -4,7 +4,21 @@ Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
 */
 
-var findMaxConsecutiveOnes = function (nums) {};
+var findMaxConsecutiveOnes = function (nums) {
+  let max = 0;
+  let curr = 0;
+
+  for (let i = 0; i < nums.length + 1; i++) {
+    if (nums[i] === 1) {
+      curr++;
+    } else {
+      max = max < curr ? curr : max;
+      curr = 0;
+    }
+  }
+
+  return max;
+};
 
 const result = findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
 console.log(result);
