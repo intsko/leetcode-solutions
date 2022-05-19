d0486e237666eed21827607df3eb0fd0beb79904
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-19 08:33:38 +0400

    init

---
+++ array-and-string/Remove-Element.js
@@ -0,0 +1,9 @@
+/*
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+*/
+
+var removeElement = function (nums, val) {};
+
+const result = removeElement([3, 2, 2, 3]);
+console.log(result);
