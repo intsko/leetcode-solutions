607ec0ba189af5b3af410c22f0d00086109ea3f2
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-19 08:44:39 +0400

    final

--- array-and-string/Remove-Element.js
+++ array-and-string/Remove-Element.js
@@ -3,7 +3,15 @@ Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 */
 
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i -= 1;
+    }
+  }
+  return nums.length;
+};
 
 const result = removeElement([3, 2, 2, 3]);
 console.log(result);
