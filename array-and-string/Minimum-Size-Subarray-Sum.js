0fa9b42f2951a94f70e5ba9a4b0ffaa7641ca33f
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-05-22 12:20:20 +0400

    init

---
+++ array-and-string/Minimum-Size-Subarray-Sum.js
@@ -0,0 +1,9 @@
+/*
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+*/
+
+var minSubArrayLen = function (target, nums) {};
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
