d55e8efe95504c6e71d9a5cc8dba58769d0e5866
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-01 12:55:22 +0400

    final

--- array-and-string/Rotate-Array.js
+++ array-and-string/Rotate-Array.js
@@ -7,6 +7,24 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 */
 
-var rotate = function (nums, k) {};
+var rotate = function (nums, k) {
+  if (k > nums.length) {
+    k = Math.round(
+      (k / nums.length - Math.floor(k / nums.length)) * nums.length
+    );
+  }
+  if (nums.length === k) {
+    return nums;
+  }
+
+  let arr = [];
+  if (nums.length > k) {
+    arr = nums.splice(nums.length - k, nums.length);
+    for (let i = arr.length - 1; i >= 0; i--) {
+      nums.unshift(arr[i]);
+    }
+  }
+  return nums;
+};
 
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
