8ee18a1024cdcc1f9dc8162b40a0f92f4b39fd4c
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-02 20:35:20 +0400

    final

--- array-and-string/Pascal's-Triangle-II.js
+++ array-and-string/Pascal's-Triangle-II.js
@@ -3,6 +3,18 @@ Input: rowIndex = 3
 Output: [1,3,3,1]
 */
 
-var getRow = function (rowIndex) {};
+var getRow = function (rowIndex) {
+  const result = [[1]];
+
+  for (let i = 1; i <= rowIndex; i++) {
+    const row = [];
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) row.push(1);
+      else row.push(result[i - 1][j - 1] + result[i - 1][j]);
+    }
+    result.push(row);
+  }
+  return result[rowIndex];
+};
 
 console.log(getRow(3));
