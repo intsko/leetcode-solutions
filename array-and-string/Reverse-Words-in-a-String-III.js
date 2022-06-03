69299a253bdcc412604fa46067c3093ec7e75e0e
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-03 09:10:31 +0400

    final

--- array-and-string/Reverse-Words-in-a-String-III.js
+++ array-and-string/Reverse-Words-in-a-String-III.js
@@ -3,6 +3,14 @@ Input: s = "Let's take LeetCode contest"
 Output: "s'teL ekat edoCteeL tsetnoc"
 */
 
-var reverseWords = function (s) {};
+var reverseWords = function (string) {
+  const arr = string.split(" ").filter((word) => word);
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split("").reverse().join("");
+  }
+
+  return arr.join(" ");
+};
 
 console.log(reverseWords("Let's take LeetCode contest"));
