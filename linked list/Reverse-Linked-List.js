8beda21ca3a57344e444ff4b71ef5489853c9de5
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-04 22:08:41 +0400

    final

--- linked list/Reverse-Linked-List.js
+++ linked list/Reverse-Linked-List.js
@@ -3,6 +3,14 @@ Input: head = [1,2,3,4,5]
 Output: [5,4,3,2,1]
 */
 
-var reverseList = function (head) {};
+var reverseList = function (head) {
+  const reversed = [];
 
-console.log(reverseList([1, 2, 3, 4, 5]));
+  for (let i = head.length - 1; i >= 0; i--) {
+    reversed.push(head[i]);
+  }
+
+  return reversed;
+};
+
+console.log(reverseList([5, 4, 3, 2, 1]));
