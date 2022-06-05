d07072474273a38914ec77bddb245ca594cad743
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-05 10:43:34 +0400

    final

--- random-problems/Palindrome-Number.js
+++ random-problems/Palindrome-Number.js
@@ -4,8 +4,15 @@ Output: true
 Explanation: 121 reads as 121 from left to right and from right to left.
 */
 
-function isPalindrome(number) {}
+function isPalindrome(number) {
+  const string = number.toString();
+  let reversedString = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return string === reversedString ? true : false;
+}
 
 console.log(isPalindrome(121)); // true
-
 console.log(isPalindrome(10)); // false
