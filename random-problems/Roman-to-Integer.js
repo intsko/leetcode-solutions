aab973cbe9e425a3d056dd36382c606e86c40b3b
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-05 10:56:24 +0400

    init

---
+++ random-problems/Roman-to-Integer.js
@@ -0,0 +1,19 @@
+/*
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+*/
+
+function romanToInt(s) {
+  const romans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+}
+
+console.log(romanToInt("III"));
