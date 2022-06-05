c1a92ae0090a85ae5237b871bbe9b20a7ae7acff
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-05 10:28:24 +0400

    init

---
+++ random-problems/Palindrome-Number.js
@@ -0,0 +1,11 @@
+/*
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+*/
+
+function isPalindrome(number) {}
+
+console.log(isPalindrome(121)); // true
+
+console.log(isPalindrome(10)); // false
