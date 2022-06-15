d9956f9ecc875dba8a3e7e07c38f87567fba17f2
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-15 13:07:08 +0400

    final

--- random-problems/Valid-Parentheses.js
+++ random-problems/Valid-Parentheses.js
@@ -7,9 +7,36 @@ Output: true
 */
 
 function isValid(s) {
-    
+    const stack = []
+
+    for (const str of s) {
+        switch (str) {
+
+            case ')': {
+                if (stack.at(-1) === '(') stack.pop()
+                else return false
+                break
+            }
+
+            case '}': {
+                if (stack.at(-1) === '{') stack.pop()
+                else return false
+                break
+            }
+
+            case ']': {
+                if (stack.at(-1) === '[') stack.pop()
+                else return false
+                break
+            }
+         
+            default: stack.push(str)
+        }
+    }
+
+    return stack.length === 0
 }
 
 console.log(
-    isValid('()')
+    isValid('(}')
 );
