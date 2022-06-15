84677c632138e9a29465bab1be2d42fa0b7d26e9
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-15 12:46:54 +0400

    final

--- random-problems/Roman-to-Integer.js
+++ random-problems/Roman-to-Integer.js
@@ -4,6 +4,28 @@ Output: 3
 Explanation: III = 3.
 */
 
-function romanToInt(s) {}
+function romanToInt(s) {
+  const romans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let sum = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (romans[s[i + 1]] > romans[s[i]]) {
+      sum += romans[s[i + 1]] - romans[s[i]];
+      i++;
+    } else {
+      sum += romans[s[i]];
+    }
+  }
+
+  return sum;
+}
 
 console.log(romanToInt("III"));
