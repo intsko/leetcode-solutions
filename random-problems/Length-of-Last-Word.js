9fab75ee5cfac076689c4b4b946124258528fb05
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-18 12:57:35 +0400

    final

--- random-problems/Length-of-Last-Word.js
+++ random-problems/Length-of-Last-Word.js
@@ -5,9 +5,10 @@ Explanation: The last word is "World" with length 5.
 */
 
 var lengthOfLastWord = function(s) {
-    
+    const arr = s.trim().split(' ')
+    return arr[arr.length - 1].length
 };
 
 console.log(
-    lengthOfLastWord("Hello World")
+    lengthOfLastWord("   fly me   to   the moon  ")
 );
