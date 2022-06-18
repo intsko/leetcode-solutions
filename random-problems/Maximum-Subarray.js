5f084101d8d78f4e3ef0a58fa97b62d9e6bf203b
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-18 12:43:13 +0400

    final

--- random-problems/Maximum-Subarray.js
+++ random-problems/Maximum-Subarray.js
@@ -5,7 +5,17 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 */
 
 var maxSubArray = function(nums) {
-    
+    let sum = 0
+    let max = nums[0]
+
+    for (let i = 0; i < nums.length; i++) {
+        
+        sum < 0 ? sum = nums[i] : sum += nums[i]
+        if (sum > max) max = sum
+
+    }
+
+    return max
 };
 
 console.log(
