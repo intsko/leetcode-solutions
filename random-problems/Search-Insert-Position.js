6c6edac9d5a5db73ba63df811faacd805d718f87
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-18 12:27:46 +0400

    final

--- random-problems/Search-Insert-Position.js
+++ random-problems/Search-Insert-Position.js
@@ -3,6 +3,16 @@ Input: nums = [1,3,5,6], target = 5
 Output: 2
 */
 
+/*
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+*/
+
+/*
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+*/
+
 var searchInsert = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
 
