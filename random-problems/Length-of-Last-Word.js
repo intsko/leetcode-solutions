4b1781accbad25c00fea94bbd2d5bd46c18573f7
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-18 12:45:41 +0400

    init

---
+++ random-problems/Length-of-Last-Word.js
@@ -0,0 +1,13 @@
+/*
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+*/
+
+var lengthOfLastWord = function(s) {
+    
+};
+
+console.log(
+    lengthOfLastWord("Hello World")
+);
