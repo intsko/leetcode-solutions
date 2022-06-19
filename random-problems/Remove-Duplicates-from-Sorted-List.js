2c7cf9e2b2b73e90dca336558915ff82a1998510
Lasha-Giorgi <intskirvelilashagiorgi@gmail.com>
2022-06-19 21:31:01 +0400

    final

--- random-problems/Remove-Duplicates-from-Sorted-List.js
+++ random-problems/Remove-Duplicates-from-Sorted-List.js
@@ -4,7 +4,15 @@ Output: [1,2,3]
 */
 
 var deleteDuplicates = function(head) {
-    
+    const unique = []
+
+    for (let i = 0; i < head.length; i++) {
+
+        if (unique.indexOf(head[i]) === -1) unique.push(head[i])
+
+    }
+
+    return unique
 };
 
 console.log(deleteDuplicates([1,1,2,3,3]));
