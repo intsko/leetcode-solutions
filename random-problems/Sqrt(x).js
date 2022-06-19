93fb2d9a6704a9e4528976195babf0e132c1b01e
Lasha-Giorgi Intskirveli <intskirvelilashagiorgi@gmail.com>
2022-06-19 18:57:39 +0400

    final

--- random-problems/Sqrt(x).js
+++ random-problems/Sqrt(x).js
@@ -4,9 +4,23 @@ Output: 2
 */
 
 var mySqrt = function(x) {
-    
+    let left = 1
+    let right = x
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2)
+        if (mid * mid > x) {
+            right = mid - 1
+        } else if (mid * mid < x) {
+            left = mid + 1
+        } else {
+            return mid
+        }
+    }
+
+    return left - 1
 };
 
 console.log(
-    mySqrt(4)
+    mySqrt(8)
 );
