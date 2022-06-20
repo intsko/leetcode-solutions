476aaf36d643e8e72d88b9c4359dd3a2fc3759e7
Lasha-Giorgi <intskirvelilashagiorgi@gmail.com>
2022-06-20 16:14:02 +0400

    final

--- random-problems/Single-Number.js
+++ random-problems/Single-Number.js
@@ -5,6 +5,14 @@ Output: 1
 
 var singleNumber = function(nums) {
     
+    nums = nums.sort()
+
+    for (let i = 0; i < nums.length; i+=2) {
+
+        if (nums[i] !== nums[i + 1]) return nums[i]
+
+    }
+
 };
 
 console.log(
