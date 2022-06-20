938be112fbe20064975c46232fa13b37e7406c5a
Lasha-Giorgi <intskirvelilashagiorgi@gmail.com>
2022-06-20 11:39:50 +0400

    final

--- random-problems/Valid-Palindrome.js
+++ random-problems/Valid-Palindrome.js
@@ -5,7 +5,12 @@ Explanation: "amanaplanacanalpanama" is a palindrome.
 */
 
 var isPalindrome = function(s) {
+    const formatted = s.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s/g, '')
+
+    const reversed = s.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replace(/\s/g, '')
+                        .split('').reverse().join('')
     
+    return formatted === reversed ? true : false 
 };
 
 console.log(
