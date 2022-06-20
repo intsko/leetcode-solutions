760c274985c74e759b2a4a204d258aceae3e6ed3
Lasha-Giorgi <intskirvelilashagiorgi@gmail.com>
2022-06-20 10:52:39 +0400

    init

---
+++ random-problems/Valid-Palindrome.js
@@ -0,0 +1,13 @@
+/*
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+*/
+
+var isPalindrome = function(s) {
+    
+};
+
+console.log(
+    isPalindrome("A man, a plan, a canal: Panama")
+);
